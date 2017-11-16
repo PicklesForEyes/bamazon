cf8c18c8c1d798442c7f2d149ee8363d67142c85
Benjamin Lawson <lawbe101@yahoo.com>
2017-11-16 14:14:48 -0500

    changed schema file name

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,4 +2,10 @@ var key = require('./keys.js');
 var mysql = require('mysql');
 var inq = require('inquirer');
 
-var connection = mysql.createConnection()
+var connection = mysql.createConnection(key);
+
+connection.connect(function(err) {
+  // if(err) throw err;
+  console.log('connected')
+  conenction.end();
+})
