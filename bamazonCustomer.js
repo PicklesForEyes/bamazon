4b79f7038928bb2f6cd1a8da35ebfaa253751472
Benjamin Lawson <lawbe101@yahoo.com>
2017-11-16 14:33:10 -0500

    started build of costumer purchase choice

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,24 @@ var inq = require('inquirer');
 var connection = mysql.createConnection(key);
 
 connection.connect(function(err) {
-  // if(err) throw err;
-  console.log('connected')
-  conenction.end();
+  if(err) throw err;
+  drawProducts();
 })
+
+function quit() {
+  connection.end();
+}
+
+function drawProducts() {
+  var query = connection.query(
+    'SELECT product_name, price, item_id FROM products',
+    function(err, res) {
+      if (err) throw err;
+      var productList = [];
+      for (var i = 0; i < res.length; i++) {
+        productList.push(res[i].product_name + ' $' + res[i].price + ', id:' + res[i].item_id);
+      }
+      console.log(productList)
+    }
+  )
+}
